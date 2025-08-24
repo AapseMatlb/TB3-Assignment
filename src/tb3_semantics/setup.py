13205187e5939fb4010274cdd1a727a3cb5ab8d1
Yashashwani Kashyap <85053075+AapseMatlb@users.noreply.github.com>
2025-08-24 22:08:49 +0530

    Update setup.py

--- src/tb3_semantics/setup.py
+++ src/tb3_semantics/setup.py
@@ -6,12 +6,11 @@ setup(
     version='0.0.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/artifacts', [
-            'artifacts/semantic_mock.json',
-        ]),
-    ],
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/artifacts', ['artifacts/semantic_mock.json']),
+    ]
+    
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Yashashwani Kashyap',
