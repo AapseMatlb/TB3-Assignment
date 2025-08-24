c616b407a2bd2d183af265a4f8e46baef65a5fda
Yashashwani Kashyap <85053075+AapseMatlb@users.noreply.github.com>
2025-08-24 21:56:32 +0530

    Packages updated

--- src/tb3_exploration/setup.py
+++ src/tb3_exploration/setup.py
@@ -4,7 +4,7 @@ package_name = 'tb3_exploration'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],  # ensure tb3_exploration/__init__.py exists
+    packages=[],  # ensure tb3_exploration/__init__.py exists
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
